23330f98264fb7747d0d31b0ff1ac16bb2dfbbec
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-03-09 21:28:50 +0100

    Added Create Tutorial

--- LearnCSharp/Areas/Admin/Controllers/TutorialController.cs
+++ LearnCSharp/Areas/Admin/Controllers/TutorialController.cs
@@ -1,7 +1,9 @@
 ﻿
 using Data;
 using Data.Models;
+using Data.Models.ViewModels;
 using Data.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -11,10 +13,13 @@ namespace LearnCSharp.Controllers
     public class TutorialController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public TutorialController(IUnitOfWork unitOfWork)
+
+        public TutorialController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
@@ -23,46 +28,66 @@ namespace LearnCSharp.Controllers
             return View(objCategoryList);
         }
 
-        
+
         //GET
         public IActionResult Upsert(int? id)
         {
-            Tutorial tutorial = new();
-            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(
-               u => new SelectListItem
-               {
-                   Text = u.Name,
-                   Value = u.Id.ToString()
-               });
-            IEnumerable<SelectListItem> SubcategoryList = _unitOfWork.Subcategory.GetAll().Select(
-               u => new SelectListItem
-               {
-                   Text = u.Name,
-                   Value = u.Id.ToString()
-               });
+            TutorialVM tutorialVM = new()
+            {
+                Tutorial = new(),
+                CategoryList = _unitOfWork.Category.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                }),
+                SubcategoryList = _unitOfWork.Subcategory.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                }),
+            };
+
             if (id == null || id == 0)
             {
                 //Create tutorial
-                return View(tutorial);
+
+                return View(tutorialVM);
             }
             else
             {
                 //Update
+
             }
 
-            return View(tutorial);
+            return View(tutorialVM);
         }
 
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Tutorial obj)
+        public IActionResult Upsert(TutorialVM obj, IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Tutorial.Update(obj);
+                //Add uploaded files to wwwroot/img/tutorials
+                string wwwRootPath = _hostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(wwwRootPath, @"img\tutorials");
+                    var extension = Path.GetExtension(file.FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        file.CopyTo(fileStreams);
+                    }
+                    obj.Tutorial.ImgUrl = @"img\tutorials" + fileName + extension;
+                }
+
+
+                _unitOfWork.Tutorial.Add(obj.Tutorial);
                 _unitOfWork.Save();
-                TempData["success"] = "Tutorial updated successfully";
+                TempData["success"] = "Tutorial created successfully";
                 return RedirectToAction("Index");
             }
             return View(obj);
@@ -90,7 +115,7 @@ namespace LearnCSharp.Controllers
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public IActionResult DeletePOST(int? id)
-            {
+        {
             var tutorialFromDb = _unitOfWork.Tutorial.GetFirstOrDefault(i => i.Id == id);
 
             if (tutorialFromDb == null)
